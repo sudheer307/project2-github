8298aa54817313880437625e3a34fc1955da34b8
sudheer <sudheer.ganatech308@gmail.com>
2019-06-12 12:14:19 +0530

    1

---
+++ src/test/java/project2/rajutest.java
@@ -0,0 +1,16 @@
+package project2;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class rajutest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	}
+
+}
